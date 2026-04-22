337dda5c558e219df81f877583f5914e6b3cf87e
Cristian <cristianchura43@gmail.com>
2026-04-22 11:04:55 -0300

    Subo ejercicios a etapá 2

---
+++ Etapa2/7_MonedasDelNivel/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MonedaOro = 10;
+            int MonedaPlata = 2;
+            Console.Write("¿Cuántas Moneras de Oro obtuviste? : ");
+            int MonedaO = int.Parse(Console.ReadLine());
+            Console.Write("¿Cuántas Monedas de Plata obtuviste? : ");
+            int MonedaPla = int.Parse(Console.ReadLine());
+            int Puntos = (MonedaOro * MonedaO) + (MonedaPlata * MonedaPla);
+            if (Puntos >= 50 && Puntos <= 99)
+            {
+                Console.WriteLine("Nivel Superado!");
+            }
+            else if (Puntos >= 100)
+            {
+                Console.WriteLine("Nivel completado con bonus!!!");
+            }
+            else
+            {
+                Console.WriteLine("Nivel insuficiente");
+            }
+            Console.WriteLine("Puntos : " + Puntos);
+            Console.ReadKey();       
+        }
+    }
+}
---
+++ Etapa2/8_AnioBisiesto/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un año : ");
+            int año = int.Parse(Console.ReadLine());
+            if (año % 4 == 0 || año % 100 == 0 && año % 400 == 0)
+            {
+                Console.WriteLine("El año es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año no es bisiesto");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
