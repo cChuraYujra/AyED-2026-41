8569b2317a8c83452364273c86c413d06d991cc2
Cristian <cristianchura43@gmail.com>
2026-05-12 11:11:29 -0300

    Actualizo ejercicio 18. Completado

--- Etapa2/18_SimuladorJuego (DESAFIO)/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Etapa2/18_SimuladorJuego (DESAFIO)/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -22,7 +22,12 @@ namespace ConsoleApplication1
             bool exploro = false;
 
             Console.WriteLine("Despiertas en una isla desconocida luego de un accidente. No tienes nada, deberas conseguir comida y explorar la isla para sobrevivir...");
-            Console.Write("\n\nPresiona cualquier tecla para continuar.");
+            Console.WriteLine("\nElije una opción del menu para avanzar con el progreso\nPor cada dia que pase se te restará 2 de hambre y 1 de vida, pero sí el hambre llega a 0 la vida se irá reduciendo en 2.\nCada opción tiene consecuencias asi que no será facíl\n\nGanas si llegas a los 10 dias con minimo 1 de vida. Pierdes si ya no te queda vida.\n \ndatos principales:");
+            Console.WriteLine("Datos del dia");
+            Console.WriteLine("Dia : " + dias + "        Comida cruda : " + comida_cruda + "     Fogata : " + fogata);
+            Console.WriteLine("Vida : " + vida + "      Comida cocida : " + comida_cocinada + "  Materiales : " + materiales);
+            Console.WriteLine("Hambre : " + hambre + "   Refugio : " + refugio);
+            Console.Write("\nPresiona cualquier tecla para continuar.");
             Console.ReadKey();
             Console.Clear();
             while (opcion != 8)
@@ -35,7 +40,7 @@ namespace ConsoleApplication1
 
                 Console.WriteLine("Datos del dia");
                 Console.WriteLine("Dia : " + dias + "        Comida cruda : " + comida_cruda + "     Fogata : " + fogata);
-                Console.WriteLine("Vida : " + vida + "      Comida cocida : " + comida_cocinada + "Materiales : " + materiales);
+                Console.WriteLine("Vida : " + vida + "      Comida cocida : " + comida_cocinada + "  Materiales : " + materiales);
                 Console.WriteLine("Hambre : " + hambre + "   Refugio : " + refugio);
 
                 Console.Write("\nEstas pensando en...\n1. Buscar comida\n2. Explorar la isla\n3. Construir un refugio\n4. Encender una fogata\n5. Cocinar comida\n6. Comer comida cocida\n7. Descansar\n8. Salir\n¿Qué haras?(Ingresar el numero que quieras hacer) : ");
@@ -44,6 +49,7 @@ namespace ConsoleApplication1
                 {
                     case 0:
                         Console.WriteLine("ERROR, ingrese un numero del menú de opciones.");
+                        Console.ReadKey();
                         Console.Clear();
                         break;
 
@@ -54,7 +60,7 @@ namespace ConsoleApplication1
                             Console.ReadKey();
                             Console.WriteLine("\nHas encontrado pedazos de comida cruda. Pero te cansaste");
                             dias++;
-                            comida_cruda = +2;
+                            comida_cruda = comida_cruda + 2;
                             vida--;
                             hambre = hambre - 2;
                         }
@@ -76,7 +82,6 @@ namespace ConsoleApplication1
                         {
                             Console.WriteLine("\nHas encontrado materiales utiles.");
                             dias++;
-                            vida--;
                             hambre = hambre - 2;
                             materiales = true;
                         }
@@ -84,7 +89,6 @@ namespace ConsoleApplication1
                         {
                             Console.WriteLine("\nNo has encontrado nada util.");
                             dias++;
-                            vida--;
                             hambre = hambre - 2;
                         }
                         else
@@ -106,14 +110,12 @@ namespace ConsoleApplication1
                             Console.WriteLine("\ncomo no tienes un refugio construyes uno.");
                             refugio = true;
                             dias++;
-                            vida--;
                             hambre = hambre - 2;
                         }
                         else
                         {
                             Console.WriteLine("\nya tienes un refugio, por lo que te quedas sin hacer nada");
                             dias++;
-                            vida--;
                             hambre = hambre - 2;
                         }
                         Console.ReadKey();
@@ -121,27 +123,26 @@ namespace ConsoleApplication1
                         break;
                     case 4:
                         Console.WriteLine("\nVas a intentar encender una fogata. Y...");
+                        Console.ReadKey();
                         if (refugio == true)
                         {
                             Console.WriteLine("\ncomo tienes un refugio haces una fogata.");
                             fogata = true;
                             dias++;
-                            vida--;
                             hambre = hambre - 2;
                         }
-                        else if (exploro == true && fogata == true)
+                        else if (exploro == true && materiales == true)
                         {
                             Console.WriteLine("\ndespués de haber explorado la isla y haber conseguido materiales decides hacer una fogata.");
                             dias++;
-                            vida--;
                             hambre = hambre - 2;
                             materiales = false;
+                            fogata = true;
                         }
                         else
                         {
                             Console.WriteLine("\ncomo te faltan cosas para poder hacer una fogata no puedes.");
                             dias++;
-                            vida--;
                             hambre = hambre - 2;
                         }
                         Console.ReadKey();
@@ -155,24 +156,98 @@ namespace ConsoleApplication1
                             comida_cocinada++;
                             comida_cruda--;
                             dias++;
-                            vida--;
                             hambre = hambre - 2;
                         }
                         else
                         {
                             Console.WriteLine("\nTe faltran cosas para cocinar la carne cruda");
                             dias++;
-                            vida--;
                             hambre = hambre - 2;
                         }
                         Console.ReadKey();
                         Console.Clear();
                         break;
+                    case 6:
+                        Console.WriteLine("\nDecides comer comida cocinada");
+                        Console.ReadKey();
+                        if (comida_cocinada > 0)
+                        {
+                            Console.WriteLine("\nComes la carne que hayas cocinado.");
+                            comida_cocinada--;
+                            hambre = hambre + 4;
+                            dias++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nNo tienes comida cocida.");
+                            dias++;
+                            hambre = hambre - 2;
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 7:
+                        Console.WriteLine("\nDecides dormir para recuperar vida");
+                        Console.ReadKey();
+                        if (refugio == true)
+                        {
+                            Console.WriteLine("\nTe duermes");
+                            vida = vida + 3;
+                            dias++;
+                            hambre = hambre - 2;
+                        }
+                        else if (refugio == false)
+                        {
+                            Console.WriteLine("\nTe duermes");
+
+                            vida++;
+                            dias++;
+                            hambre = hambre - 2;
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 8:
+                        Console.WriteLine("\nTe sales del juego. Hasta luego.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    default:
+                        Console.WriteLine("ERROR, ingrese un numero del menú de opciones.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+
+                if (hambre < 0)
+                {
+                    vida = vida - 2;
+                    hambre = 0;
+                }
+                if (vida > 10)
+                {
+                    vida = 10;
+                }
+                else if (vida < 0)
+                {
+                    vida = 0;
+                }
+                if(vida < 1)
+                {
+                    Console.WriteLine("PERDISTE");
+                    Console.ReadKey();
+                    Console.Clear();
+                    opcion = 8;
+                }
+                else if (dias == 10 && vida > 0)
+                {
+                    Console.WriteLine("GANASTE");
+                    Console.ReadKey();
+                    Console.Clear();
+                    opcion = 8; 
                 }
             }
-
-
-            Console.ReadKey();
         }
     }
 }
