bfc37c55f842682d87863a73931e84a5a656a311
Cristian <cristianchura43@gmail.com>
2026-06-02 11:31:19 -0300

    Creo Etapa3 y subo ejercicios 0 y 1

---
+++ Etapa3/0_CargandoEdades/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int[] edades = new int[5];
+            for (int i = 0; i < 5; i++ , num++)
+            {
+                Console.Write("Alumno " + num + " : " );
+                edades[i] = int.Parse(Console.ReadLine());
+            }
+            for(int a = 0; a < 5; a++)
+            {
+                Console.Write(edades[a] + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
